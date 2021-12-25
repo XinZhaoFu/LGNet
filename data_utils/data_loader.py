83739846b18c2479b89c67c0e905f7f8fddb23ab
XinZhaoFu <35882457+XinZhaoFu@users.noreply.github.com>
2021-12-25 18:19:43 +0800

    update

--- data_utils/data_loader.py
+++ data_utils/data_loader.py
@@ -1,4 +1,5 @@
 from data_utils.utils import get_specific_type_file_list, shuffle_file, file_consistency_check
+import tensorflow as tf
 
 
 class Data_Loader_File:
@@ -69,5 +70,33 @@ def _data_preprocess(img_file_path,
         label_file_list.sort()
         img_file_list, label_file_list = shuffle_file(img_file_list, label_file_list)
 
+    datasets = tf.data.Dataset.from_tensor_slices((img_file_list, label_file_list))
+    datasets = datasets.map(_load_and_preprocess_onehot_datasets, num_parallel_calls=tf.data.AUTOTUNE)
+    datasets = datasets.shuffle(buffer_size=batch_size * 8)
+    datasets = datasets.batch(batch_size=batch_size)
+    datasets = datasets.prefetch(buffer_size=tf.data.AUTOTUNE)
 
-    return 0
+    return datasets
+
+
+def _load_and_preprocess_onehot_datasets(img_path, label_path):
+    """
+    对img和label进行读取预处理 其中label以onehot的形式
+
+    :param img_path:
+    :param label_path:
+    :return:
+    """
+    image = tf.io.read_file(img_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.reshape(tensor=image, shape=(512, 512))
+    image = tf.cast(image, tf.float32) / 255.0
+
+    label = tf.io.read_file(label_path)
+    label = tf.image.decode_png(label, channels=1)
+    label = tf.reshape(tensor=label, shape=(512, 512))
+
+    label = tf.cast(label, dtype=tf.uint8)
+    label = tf.one_hot(indices=label, depth=3)
+
+    return image, label
